d37aa266c1925a8b05b82c55363edd7be1e31451
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-04-16 23:13:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var someArr = [];
+  for(let i = 0; i < musicians.length; i++){
+    var newStr = `${musicians[i]} plays ${instruments[i]}`;
+    someArr.push(newStr);
+  }
+  return someArr;
+}
